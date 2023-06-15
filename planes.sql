f77327d79d0b962df8157ad4bf8f857d6e2c72f6
Tomas Mendizabal <98746556+100461170@users.noreply.github.com>
2023-06-15 12:20:25 -0500

    Create planes.sql

---
+++ planes.sql
@@ -0,0 +1,46 @@
+explain plan for 
+UPDATE tracks
+  set lyrics = dbms_random.string('a',dbms_random.value(900,1200))
+  where searchk= 'M3867FZK78368IU//10'; 
+  pkg_costes.run_test(10);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+
+explain plan for
+WITH authors as (select title,writer, writer musician from songs
+ UNION select title,writer,cowriter musician from songs),
+ authorship as (select distinct band performer, title, writer, 1 flag
+ FROM involvement join authors using(musician) ),
+ recordings as (select performer,tracks.title,writer
+ from albums join tracks using(pair)),
+ recs_match as (select performer,
+ round(sum(flag)*100/count('c'),2) pct_recs
+ from recordings left join authorship
+ using(performer,title,writer)
+ group by performer),
+ pers_match as (select performer,
+ round(sum(flag)*100/count('c'),2) pct_pers
+ from (select performer, songtitle title,
+ songwriter writer
+ from performances) P
+ left join authorship
+ using(performer,title,writer)
+ group by performer)
+SELECT performer, pct_recs, pct_pers
+ from recs_match full join pers_match using(performer);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+
+explain plan for
+WITH recordings as (select performer,tracks.title, writer, min(rec_date) rec, 1 token from albums join tracks using(pair) 
+group by performer,tracks.title,writer), playbacks as (select P.performer, sum(token)*100/count('x') percentage,
+avg(nvl2(rec,when-rec,rec)) as age FROM performances P left join recordings R on(P.performer=R.performer
+ AND R.title=P.songtitle
+ AND R.writer=P.songwriter
+ AND P.when>R.rec)
+ GROUP BY P.performer
+ ORDER BY percentage desc)
+SELECT performer, percentage, floor(age/365.2422) years,
+ floor(mod(age,365.2422)/30.43685) months,
+ floor(mod(age,365.2422)-
+(floor(mod(age,365.2422)/30.43685)*30.43685)) days
+ FROM playbacks WHERE rownum<=10;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
